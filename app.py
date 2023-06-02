40a7ce9c00672e7ac273cfd3c45340fc33db2564
Fernanda Santos <ftspublicidade@gmail.com>
2023-06-02 17:37:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def main():
     
 
     # Criação do gráfico de Treemap
-    fig3 = px.treemap(total_idade, path=['Total'], values=['Count'], title="Total de Vacinados por Faixa Etária",
+    fig3 = px.treemap(total_idade, path=[df.iloc[:, 0].tolist()], values=[df.iloc[:, 1].tolist()], title="Total de Vacinados por Faixa Etária",
                 color_discrete_sequence=px.colors.qualitative.Dark2)
     # Atualizar o layout para centralizar o título
     fig3.update_layout(title_x=0.5)
