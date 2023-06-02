c105b052b34e1a7057031b57a491635066a44d53
Fernanda Santos <ftspublicidade@gmail.com>
2023-06-02 17:52:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -78,17 +78,15 @@ def main():
                             (dados_filtrados["faixa_etaria"] != "54 - JANSSEN COVID-19 VACCINE (JOHNSON & JOHNSON)") &
                            (dados_filtrados["faixa_etaria"] != "FEMININO")]
 
-    total_idade = total_faixa_etaria["faixa_etaria"].value_counts().reset_index()
-    st.write(total_idade)
-    st.write(total_idade["faixa_etaria"])
-    count = total_idade['count']
-    st.write(count)
+    total_idade = total_faixa_etaria["faixa_etaria"].value_counts()
+    st.write(total_idade.index)
+    st.write(total_idade.values)
     
     
     
 
     # Criação do gráfico de Treemap
-    fig3 = px.treemap(total_idade, path=["faixa_etaria"], values=[count], title="Total de Vacinados por Faixa Etária",
+    fig3 = px.treemap(total_idade, path=[total_idade.index], values=total_idade.values, title="Total de Vacinados por Faixa Etária",
                 color_discrete_sequence=px.colors.qualitative.Dark2)
     # Atualizar o layout para centralizar o título
     fig3.update_layout(title_x=0.5)
